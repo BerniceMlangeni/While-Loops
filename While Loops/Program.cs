3d4b58eab780b8926cd87f362824ab2b4379c888
BerniceMlangeni <bernicemlangeni@gmail.com>
2024-08-01 15:22:49 +0200

    Add project files.

---
+++ While Loops/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace While_Loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+              string? UserInput = "";
+
+              while(UserInput.ToLower() != "exit")
+             {
+                 Console.WriteLine("Type exit to quit");
+                 UserInput= Console.ReadLine();
+              }
+
+             Console.WriteLine("Program terminated.");
+
+
+        }
+    }
+}
